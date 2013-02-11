be1c7795609282ecf075cf9372a289fbfee18473
Sate <markc@Marks-MacBook-Air.local>
2013-02-11 12:40:31 -0800

    automated clone from private org repo (history omitted for technical reasons)

---
+++ src/PlayerView.js
@@ -0,0 +1,32 @@
+var PlayerView = Backbone.View.extend({
+
+  initialize: function(){
+    // set up an event listener on the songs collection
+    this.collection.on('change', this.handleQueueChange, this);
+    this.collection.on('play', this.play, this);
+  },
+
+  // templates are a nicer way to put js data into html strings
+  template: _.template('<audio src="<%= url %>" controls autoplay></audio>'),
+
+  render: function(){
+    if(this.model){
+      this.$el.html(this.template(this.model.attributes));
+    }
+    return this.$el;
+  },
+
+  play: function(model) {
+    this.model = model;
+    this.render();
+  },
+
+  // event listener
+  handleQueueChange: function(){
+    if(!this.model){
+      this.model = this.collection.queued()[0];
+      this.render();
+    }
+  }
+
+});
---
+++ src/PlaylistView.js
@@ -0,0 +1,11 @@
+var PlaylistView = Backbone.View.extend({
+
+  initialize: function(){
+  },
+
+  render: function(){
+    // modify the view's .$el here
+    return this.$el;
+  }
+
+});
